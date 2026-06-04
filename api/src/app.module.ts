c42b56b10f8d0774e0da106dc24be4a230c62a6e
Vasco-C-Loureiro <vasco.v.loureiro@gmail.com>
2026-06-04 12:16:24 +0100

    feat: firmware upload, download, list and state machine

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
 import { VariantsModule } from './variants/variants.module';
+import { FirmwareModule } from './firmware/firmware.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule, VariantsModule],
+  imports: [PrismaModule, ProductsModule, VariantsModule, FirmwareModule],
 })
 export class AppModule {}
